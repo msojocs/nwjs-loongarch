b3012eb7b2baa608fe27dc6d966038762566d8aa
msojocs <jiyecafe@gmail.com>
2023-12-12 22:20:22 +0800

    feat: sysroot解压

--- tools/sysroot.sh
+++ tools/sysroot.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-mkdir -p /opt/loongarch64/sysroot/usr
-mkdir ./sysroot
-mount clfs-loongarch64-system-8.1-sysroot.squashfs ./sysroot
-cp -raf ./sysroot/usr/include ./sysroot/usr/lib ./sysroot/usr/lib64 \
-        ./sysroot/usr/share /opt/loongarch64/sysroot/usr
-cp -raf ./sysroot/lib ./sysroot/lib64 /opt/loongarch64/sysroot/
-umount ./sysroot
+set -e
+
+root_dir=$(cd `dirname $0`/.. && pwd -P)
+success() {
+  echo -e "\033[42;37m 成功 \033[0m $1"
+}
+notice() {
+  echo -e "\033[36m $1 \033[0m "
+}
+fail() {
+  echo -e "\033[41;37m 失败 \033[0m $1"
+}
+
+sysroot_name="clfs-loongarch64-system-8.1-sysroot.squashfs"
+# 下载
+if [ ! -f "$root_dir/$sysroot_name" ];then
+  wget https://github.com/loongson/build-tools/releases/download/2023.08.08/clfs-loongarch64-system-8.1-sysroot.squashfs
+fi
+# 解压
+if [ -z "$(ls -A $root_dir/sysroot)" ];then
+  unsquashfs -d sysroot -no-xattrs clfs-loongarch64-system-8.1-sysroot.squashfs usr/include usr/lib usr/lib64 usr/share lib lib64
+fi
