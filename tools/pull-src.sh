cc5b41b64d28eaf5ce040da0d065bd2315ebde84
msojocs <jiyecafe@gmail.com>
2025-12-24 20:33:28 +0800

    feat: nw94支持

--- tools/pull-src.sh
+++ tools/pull-src.sh
@@ -93,6 +93,7 @@ if read -t 60 -p "execute 'gclient sync -D'? (Y/N):" name    # -t，设置输入
 then                                              # 如果不超过5秒
   if [ "y" = "$name" ] || [ "Y" = "$name" ];then
     gclient sync -D
+    "$root_dir/tools/sync-reset.sh"
   fi
 else                                              # 超过5秒
     echo "Timeout"
