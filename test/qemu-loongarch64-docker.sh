7c6105876a3333fafc862530dcdf225779c63912
msojocs <jiyecafe@gmail.com>
2026-01-10 06:24:25 +0800

    update:更新qemu脚本

--- test/qemu-loongarch64-docker.sh
+++ test/qemu-loongarch64-docker.sh
@@ -3,24 +3,26 @@ set -ex
 
 root_dir=$(cd `dirname $0`/.. && pwd -P)
 
-docker run --rm --privileged tonistiigi/binfmt:qemu-v9.2.2-52 --install all
+docker run --rm --privileged loongcr.lcpu.dev/multiarch/archlinux --reset -p yes
 # apt update && apt install -y gdb build-essential libncurses-dev
+# apt install libnss3-dev libatk1.0-dev libcups2-dev libxkbcommon-dev libxcomposite-dev libxdamage-dev libxrandr-dev libpango1.0-dev libdrm-dev libgbm-dev libasound2-data libasound2-dev
 container_name="loongarchlinux-test"
 # 检查docker 容器是否存在
 if [ "$(docker ps -a | grep $container_name)" ]; then
     docker start $container_name
     cd $root_dir/output/dist_nwjs
-    version="0.94.1"
+    version="0.94.0"
     name="nwjs-sdk-v$version-linux-loong64"
     if [ ! -d "$name" ]; then
         tar -zxf $name.tar.gz
     fi
-    docker exec -it $container_name bash -c "ls -al ~/.config && rm -rf ~/.config/* && /workspace/source-code/nwjs/src/out/nw/nw --disable-gpu"
+    docker exec -it $container_name bash # -c "rm -rf ~/.config/* && /workspace/source-code/nwjs/src/out/nw/nw --disable-gpu"
     # docker exec -it $container_name /workspace/output/dist_nwjs/$name/nw --disable-gpu
     # gdb --args /workspace/output/dist_nwjs/nwjs-v0.94.1-linux-loong64/nw --disable-seccomp-sandbox http://google.com
 else
     docker run -it \
         --name "$container_name" \
+        --platform="linux/loong64" \
         --cap-add SYS_PTRACE \
         --security-opt seccomp=unconfined \
         --env="DISPLAY" \
@@ -28,7 +30,7 @@ else
         --volume="/tmp/.X11-unix:/tmp/.X11-unix:ro" \
         -v "$root_dir":/workspace \
         -w /workspace \
-        jiegec/loong64-debian:latest \
-        bash
+        ghcr.io/loongarchlinux/archlinux:latest \
+        pacman -Sy --noconfirm nss atk cups libxkbcommon libxcomposite libxdamage libxrandr pango alsa-lib mesa
 fi
-# pacman -S --noconfirm nss atk cups libxkbcommon libxcomposite libxdamage libxrandr pango alsa-lib
+# pacman -Sy --noconfirm mesa nss atk cups libxkbcommon libxcomposite libxdamage libxrandr pango alsa-lib
